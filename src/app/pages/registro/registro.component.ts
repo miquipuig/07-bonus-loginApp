e355984a90d4d3695082ba6ed41d623304d242fb
Miquel Puig <mpuig22@gmail.com>
2019-07-21 23:34:21 +0200

    aaa

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.models';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -14,12 +15,15 @@ export class RegistroComponent implements OnInit {
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
-    this.usuario.email = 'mpuig22@gmail.com';
+    // this.usuario.email = 'mpuig22@gmail.com';
    }
 
-   onSubmit() {
+   onSubmit( form: NgForm) {
+    
+    if (form.invalid) {return;}
      console.log('Formulario enviado');
      console.log(this.usuario);
+     console.log(form);
    }
 
 
